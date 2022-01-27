0c2772ece96abde4b80987fb726ce3e5c8c81f1a
Violet Stewart <vkazoo2@gmail.com>
2022-01-27 14:17:00 -0800

    set up services files and app js

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import { getFilms } from './services/films';
+import FilmList from '../components/FilmList/FilmList';
+import Controls from '../components/Controls/Controls';
+
+function App() {
+  const [films, setFilms] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getFilms();
+      setFilms(data.results);
+      setLoading(false);
+    };
+    if (loading) {
+      fetchData();
+    }
+  }, [loading]);
+
+  if (loading) return <h1>LOADING</h1>;
+
+  return (
+    <div className="App">
+      <h1>Film's By Studio Ghibli</h1>
+      <Controls />
+      <FilmList films={films} loading={loading} />
+    </div>
+  );
+}
+
+export default App;
